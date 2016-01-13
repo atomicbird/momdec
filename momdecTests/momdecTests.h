69d131d4d9e1b7adf00d1f8b67a3e9d546a3f0ad
Steve Moser <contact@stevemoser.org>
2016-01-13 12:00:32 -0500

    Convert from OCUnit to XCTest

--- momdecTests/momdecTests.h
+++ momdecTests/momdecTests.h
@@ -6,8 +6,8 @@
 //  Copyright (c) 2013 Tom Harrington. All rights reserved.
 //
 
-#import <SenTestingKit/SenTestingKit.h>
+#import <XCTest/XCTest.h>
 
-@interface momdecTests : SenTestCase
+@interface momdecTests : XCTestCase
 
 @end
